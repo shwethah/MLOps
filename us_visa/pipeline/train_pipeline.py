da5d6a6f0c5e0dc2b5d3d2727d153c62a121b101
Shwetha.H <h.shwetha53@gmail.com>
2024-04-10 22:54:30 -0500

    Updated

--- us_visa/pipeline/train_pipeline.py
+++ us_visa/pipeline/train_pipeline.py
@@ -9,16 +9,16 @@ from us_visa.components.data_transformation import DataTransformation
 # from us_visa.components.model_evaluation import ModelEvaluation
 # from us_visa.components.model_pusher import ModelPusher
 
-from us_visa.entity.config_entity import (DataIngestionConfig)
-                                        #   DataValidationConfig,
+from us_visa.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig,)
                                         #   DataTransformationConfig,
                                         #   ModelTrainerConfig,
                                         #   ModelEvaluationConfig,
                                         #   ModelPusherConfig)
                                           
 
-from us_visa.entity.artifact_entity import (DataIngestionArtifact)
-                                            # DataValidationArtifact,
+from us_visa.entity.artifact_entity import (DataIngestionArtifact,
+                                            DataValidationArtifact)
                                             # DataTransformationArtifact,
                                             # ModelTrainerArtifact,
                                             # ModelEvaluationArtifact,
@@ -29,7 +29,7 @@ from us_visa.entity.artifact_entity import (DataIngestionArtifact)
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        # self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         # self.data_transformation_config = DataTransformationConfig()
         # self.model_trainer_config = ModelTrainerConfig()
         # self.model_evaluation_config = ModelEvaluationConfig()
@@ -56,29 +56,29 @@ class TrainPipeline:
         
     
 
-    # def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting data validation component
-    #     """
-    #     logging.info("Entered the start_data_validation method of TrainPipeline class")
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
-    #     try:
-    #         data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-    #                                          data_validation_config=self.data_validation_config
-    #                                          )
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
 
-    #         data_validation_artifact = data_validation.initiate_data_validation()
+            data_validation_artifact = data_validation.initiate_data_validation()
 
-    #         logging.info("Performed the data validation operation")
+            logging.info("Performed the data validation operation")
 
-    #         logging.info(
-    #             "Exited the start_data_validation method of TrainPipeline class"
-    #         )
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
 
-    #         return data_validation_artifact
+            return data_validation_artifact
 
-    #     except Exception as e:
-    #         raise USvisaException(e, sys) from e
+        except Exception as e:
+            raise USvisaException(e, sys) from e
         
 
 
@@ -158,7 +158,7 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             # data_transformation_artifact = self.start_data_transformation(
             #     data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
             # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
