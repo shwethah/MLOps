89377ced86be95b977fdf0ca460960b0eeee5c34
Shwetha.H <h.shwetha53@gmail.com>
2024-04-10 23:51:21 -0500

    Model Trainer

--- us_visa/pipeline/train_pipeline.py
+++ us_visa/pipeline/train_pipeline.py
@@ -5,22 +5,22 @@ from us_visa.logger import logging
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
-# from us_visa.components.model_trainer import ModelTrainer
+from us_visa.components.model_trainer import ModelTrainer
 # from us_visa.components.model_evaluation import ModelEvaluation
 # from us_visa.components.model_pusher import ModelPusher
 
 from us_visa.entity.config_entity import (DataIngestionConfig,
                                           DataValidationConfig,
-                                          DataTransformationConfig)
-                                        #   ModelTrainerConfig,
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig)
                                         #   ModelEvaluationConfig,
                                         #   ModelPusherConfig)
                                           
 
 from us_visa.entity.artifact_entity import (DataIngestionArtifact,
                                             DataValidationArtifact,
-                                            DataTransformationArtifact)
-                                            # ModelTrainerArtifact,
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact)
                                             # ModelEvaluationArtifact,
                                             # ModelPusherArtifact)
 
@@ -31,7 +31,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
-        # self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
         # self.model_evaluation_config = ModelEvaluationConfig()
         # self.model_pusher_config = ModelPusherConfig()
 
@@ -99,19 +99,19 @@ class TrainPipeline:
         
 
     
-    # def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting model training
-    #     """
-    #     try:
-    #         model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
-    #                                      model_trainer_config=self.model_trainer_config
-    #                                      )
-    #         model_trainer_artifact = model_trainer.initiate_model_trainer()
-    #         return model_trainer_artifact
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
 
-    #     except Exception as e:
-    #         raise USvisaException(e, sys)
+        except Exception as e:
+            raise USvisaException(e, sys)
         
     
 
@@ -161,7 +161,7 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             # model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
             #                                                         model_trainer_artifact=model_trainer_artifact)
             
