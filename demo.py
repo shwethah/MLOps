a4e9f9597e3e8d09a0fa7239b4e7ba2a07128e05
Shwetha.H <h.shwetha53@gmail.com>
2024-04-10 23:51:04 -0500

    Model Trainer

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
 import warnings
-
+import time
 warnings.filterwarnings("ignore")
 from us_visa.pipeline.train_pipeline import TrainPipeline
 
+start = time.time()
 pipline  = TrainPipeline()
 pipline.run_pipeline()
+end  = time.time()
+print(f"Total time taken: {end - start} seconds")
